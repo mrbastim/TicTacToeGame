206dd083690e5f91a60ca9ce8609770c10390e9a
bas <tima.bashirov92@gmail.com>
2025-04-03 23:30:00 +0300

    Add game and history fragments with layout updates

--- app/src/main/java/com/example/tictactoegame/MainActivity.kt
+++ app/src/main/java/com/example/tictactoegame/MainActivity.kt
@@ -1,20 +1,33 @@
 package com.example.tictactoegame
 
 import android.os.Bundle
+import android.widget.FrameLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+
+
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var frameLayout: FrameLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.fragment_menu)
+        setContentView(R.layout.fragment_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        frameLayout = findViewById(R.id.frame_layout)
+        // Set the initial fragment to MenuFragment
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.frame_layout, MenuFragment())
+            .commit()
+        // Set the background color of the FrameLayout to transparent
+        frameLayout.setBackgroundColor(getColor(R.color.transparent))
     }
 }
